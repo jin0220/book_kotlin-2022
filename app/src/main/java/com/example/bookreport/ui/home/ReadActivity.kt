9c74a6350818c814a2350f600da7401e257fdaed
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-01-13 17:02:24 +0900

    read, write 페이지 ui 추가 및 수정

--- app/src/main/java/com/example/bookreport/ui/home/ReadActivity.kt
+++ app/src/main/java/com/example/bookreport/ui/home/ReadActivity.kt
@@ -2,6 +2,8 @@ package com.example.bookreport.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.bookreport.databinding.ActivityReadBinding
 
 class ReadActivity : AppCompatActivity() {
@@ -11,6 +13,18 @@ class ReadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/bookreport/ui/home/WriteActivity.kt
+++ app/src/main/java/com/example/bookreport/ui/home/WriteActivity.kt
@@ -2,6 +2,7 @@ package com.example.bookreport.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.bookreport.databinding.ActivityWriteBinding
 
 class WriteActivity : AppCompatActivity() {
@@ -10,5 +11,20 @@ class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
