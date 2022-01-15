04c3e5dd1795085b519ab677580d03ed38827a12
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-01-15 20:52:13 +0900

    write 페이지 일부 ui 수정

--- app/src/main/java/com/example/bookreport/ui/home/WriteActivity.kt
+++ app/src/main/java/com/example/bookreport/ui/home/WriteActivity.kt
@@ -1,9 +1,12 @@
 package com.example.bookreport.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bookreport.databinding.ActivityWriteBinding
+import java.util.*
 
 class WriteActivity : AppCompatActivity() {
     val binding by lazy { ActivityWriteBinding.inflate(layoutInflater) }
@@ -15,6 +18,22 @@ class WriteActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolBar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val calendar: Calendar = Calendar.getInstance()
+        var year = calendar.get(Calendar.YEAR)
+        var month = calendar.get(Calendar.MONTH)
+        var day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        binding.dateSet.text = "$year-${String.format("%02d", (month + 1))}-${String.format("%02d", day)}"
+
+        binding.dateSet.setOnClickListener {
+            DatePickerDialog(this, AlertDialog.THEME_HOLO_LIGHT, listener, year, month, day).show()
+        }
+    }
+
+
+    private var listener = DatePickerDialog.OnDateSetListener { p0, year, month, day ->
+        binding.dateSet.text = "$year-${String.format("%02d", (month + 1))}-${String.format("%02d", day)}"
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
