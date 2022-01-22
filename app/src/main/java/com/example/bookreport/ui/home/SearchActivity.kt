29b2d2639a9af5b55582727fc11eef5f4eef1a94
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-01-22 15:44:11 +0900

    API 연동
    api 데이터 recyclerview에 연결

--- app/src/main/java/com/example/bookreport/ui/home/SearchActivity.kt
+++ app/src/main/java/com/example/bookreport/ui/home/SearchActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookreport.databinding.ActivitySearchBinding
 
@@ -23,12 +24,13 @@ class SearchActivity : AppCompatActivity() {
         binding.searchResult.adapter = adapter
         binding.searchResult.layoutManager = LinearLayoutManager(this)
 
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager(this).orientation)
+        binding.searchResult.addItemDecoration(dividerItemDecoration)
+
         searchViewModel.myResponse.observe(this, Observer {
             if(it.isSuccessful){
-//                Log.d("확인", it.body().toString())
-                Log.d("확인", it.body().toString())
-                Log.d("확인", it.body()?.items?.get(0)?.title.toString())
-                Log.d("확인", it.body()?.items.toString())
+                adapter.list = it.body()?.items!!
+                adapter.notifyDataSetChanged()
             }
             else{
                 Log.d("확인", "실패")
--- app/src/main/java/com/example/bookreport/ui/home/SearchAdapter.kt
+++ app/src/main/java/com/example/bookreport/ui/home/SearchAdapter.kt
@@ -5,25 +5,31 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookreport.data.model.Book
-import com.example.bookreport.databinding.ItemListBinding
+import com.example.bookreport.databinding.ItemSearchBinding
+import com.squareup.picasso.Picasso
 
 class SearchAdapter: RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
-    var list: MutableList<Book> = mutableListOf()
+    var list: List<Book.Items> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        return holder.bind()
+        return holder.bind(list[position])
     }
 
     override fun getItemCount(): Int = list.size
 
-    inner class ViewHolder(val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(){
-            Log.d("확인", list.toString())
+    inner class ViewHolder(val binding: ItemSearchBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(book: Book.Items){
+            with(binding) {
+                Picasso.get().load(book.image).into(image)
+                title.text = book.title
+                author.text = book.author
+                publisher.text = book.publisher
+            }
         }
     }
 }
